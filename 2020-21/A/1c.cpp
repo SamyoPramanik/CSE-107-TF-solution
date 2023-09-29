174256283c5a108445c8f4a300c9a920a66eb92f
Mohammad Raihan Rashid <86001046+raihanrsd@users.noreply.github.com>
2023-09-29 15:19:37 +0600

    Update 1c.cpp
    Some corrections

--- 2020-21/A/1c.cpp
+++ 2020-21/A/1c.cpp
@@ -1,56 +1,46 @@
 #include <iostream>
 #include <string>
+#include <list>
+
 using namespace std;
 
-class tollbooth
-{
-private:
-    string regNumber[100];
-    unsigned int totalCars;
-    double totalMoney;
-
-public:
-    // getter
-    unsigned int getTotalCars()
-    {
-        return totalCars;
+class tollbooth{
+    list<string> lst;
+    u_int cars;
+    double amount;
+    public:
+    tollbooth(){
+        cars = amount = 0;
     }
 
-    double getTotalMoney()
-    {
-        return totalMoney;
+    list<string> getList() const {
+        return lst;
     }
 
-    // constructor
-    tollbooth()
-    {
-        totalCars = 0;
-        totalMoney = 0;
+    double getAmount() const{
+        return amount;
     }
 
-    // to add a car who paid the toll
-    void payingCar()
-    {
-        totalCars++;
-        totalMoney += 0.5;
+    u_int getCars() const{
+        return cars;
+    }
+    // no setter should be needed as the system is automated and adding setter options
+    // might make it vulnerable to corrupted information
+    void payingCar(string registration_no){
+        cars++;
+        amount += 0.5;
+        lst.push_back(registration_no);
     }
 
-    // to add a car who didn't pay the toll
-    void nopayCar()
-    {
-        totalCars++;
+    void noPayingCar(string registration_no){
+        cars++;
+        lst.push_back(registration_no);
     }
 
-    void display()
-    {
-        cout << "Car registration numbers: " << endl;
-
-        for (int i = 0; i < totalCars; i++)
-        {
-            cout << regNumber[i] << endl;
+    void display() const{
+        for(auto itr: lst){
+            cout << itr << endl;
         }
-
-        cout << "Total cars: " << totalCars << endl;
-        cout << "Total money: " << totalMoney << endl;
+        cout << "Total Cars : " << cars << endl << "Total amount: " << amount << endl;
     }
 };
