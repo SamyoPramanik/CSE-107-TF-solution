9db5c0e033fe6ba6311551a021c456535a11dc22
Samyo Pramanik <121597178+SamyoPramanik@users.noreply.github.com>
2023-09-29 11:12:31 +0000

    correction in 2018-19 4c

--- 2018-19/A/4c.cpp
+++ 2018-19/A/4c.cpp
@@ -12,23 +12,26 @@ public:
         month = m;
         year = y;
     }
-    int operator-(Date& temp)
+    int operator-(Date &temp)
     {
-        int days = abs(day - temp.day)+abs(month - temp.month)*30+abs(year - temp.year)*365;
+        int days = abs(day - temp.day + (month - temp.month) * 30 + (year - temp.year) * 360);
+        return days;
     }
-    friend ostream& operator<<(ostream& out, Date& temp);
+
+    friend ostream &operator<<(ostream &out, Date &temp);
 };
 
-ostream& operator<<(ostream& out, Date& temp)
+ostream &operator<<(ostream &out, Date &temp)
 {
-    out << temp.day << "-" << temp.month << "-" << temp.year;  // "out"
+    out << temp.day << "-" << temp.month << "-" << temp.year; // "out"
     return out;
 }
 
 int main()
 {
-    Date d1(1, 1, 2000), d2(1, 1, 2001);
+    Date d1(1, 1, 2012), d2(12, 30, 2011);
     cout << "Difference between two dates is: " << d1 - d2 << endl;
-    cout << d1 << endl;     
+    cout << d1 << endl;
+    cout<<d2<<endl;
     return 0;
 }
